285d4d00b58ec7c5fb56325cb5b17d514d173fce
Mouheb <mhamdi.mouheb@esprit.tn>
2022-03-09 11:40:56 +0100

    integration

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,23 +6,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import NavHead from './components/NavHead';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/pages/Login';
-import Admin from './components/pages/Admin';
-import NotFounds from './components/pages/NotFound';
 function App() {
   return (
     <>
       <Router>
-        <Navbar  />
+        <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/login' component={Login} />
           <Route path='/sign-up' component={SignUp} />
-          <Route path='/Eboard/auth/admin' component={Admin} />
-          <Route path='**' component={NotFounds} />
         </Switch>
       </Router>
     </>
