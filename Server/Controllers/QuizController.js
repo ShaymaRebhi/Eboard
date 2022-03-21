2c77494db8de791ddb2732c4c6325f3a46f43729
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-03-21 22:56:40 +0100

    Add Quiz with a question and an option

--- Server/Controllers/QuizController.js
+++ Server/Controllers/QuizController.js
@@ -4,19 +4,35 @@ const Question = require('../Model/QuestionQuiz')
 
 
 exports.AddQuiz = async(req,res)=>{
-    const _Quiz = new Quiz(req.body);
-    const _Option = new Option(req.body);
-    const _Question = new Question(req.body);
-    _Option.save((err,option)=> {
-        if (err) return res.status(503).json({error: err});
-        req.body.option= option;
-            _Question.save((err, question) => {
-                if (err) return res.status(503).json({error: err});
-                req.body.Question = question;
-                _Quiz.save((err, quiz) => {
+
+    const _Option = new Option({
+        OptionText : req.body.OptionText,
+        IsValid :req.body.IsValid
+    });
+
+   await _Option.save();
+    const optionID = [_Option._id];
+    const _Question = new Question({
+        questionText : req.body.questionText,
+        Required :req.body.Required,
+        Score :req.body.Score,
+        Options :optionID,
+    });
+
+    await _Question.save();
+    const questionID = _Question._id;
+    const _Quiz = new Quiz({
+        Title : req.body.Title,
+        Class :req.body.Class,
+        Description :req.body.Description,
+        Questions :questionID,
+    });
+    await _Quiz.save((err, quiz) => {
                         if (err) return res.status(503).json({error: err});
-                        if (quiz) return res.status(200).json(quiz);
-                    })
-             })
+                        if (quiz) return res.status(200).json({
+                            success:true,
+                            id:quiz._id,
+                            message:'quiz Created',
+                        });
     })
 }
