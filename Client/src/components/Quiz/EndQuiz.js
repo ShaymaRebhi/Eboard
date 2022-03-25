774c03b0a321e517dac22421df7daaeb4e03eb83
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-03-25 13:07:03 +0100

    update props of display Quiz

--- Client/src/components/Quiz/EndQuiz.js
+++ Client/src/components/Quiz/EndQuiz.js
@@ -3,7 +3,7 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Accordion from "@material-ui/core/Accordion";
 import "./EndQuiz.css";
 
-function EndQuiz({results, data, onAnswersCheck, time}) {
+function EndQuiz(props) {
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [nbCorrectAnswers, setNbCorrectAnswers] = useState(0);
   const [scoreTotal, setScoreTotal] = useState(0);
@@ -23,14 +23,14 @@ function EndQuiz({results, data, onAnswersCheck, time}) {
     let correct = 0;
     let score = 0;
     let optioncorrect = "";
-    console.log(results)
-    data.forEach((op,i)=> {
+    console.log(props.results)
+    props.data.forEach((op,i)=> {
       score = score + op.score
 
     })
     setScoreTotal(score)
     console.log(score)
-    results.forEach((result, index) => {
+    props.results.forEach((result, index) => {
       result.b.map((op,i)=>(
       optioncorrect=op.optionText
       ))
@@ -53,11 +53,11 @@ function EndQuiz({results, data, onAnswersCheck, time}) {
             <div className="card-content-Quiz">
               <div className="content-Quiz">
                 <h3 className="resultsQuiz">Your Results</h3>
-                <p className="scoreQuiz">{nbCorrectAnswers} of {data.length}</p>
+                <p className="scoreQuiz">{nbCorrectAnswers} of {props.data.length}</p>
                 <p className="scoreQuiz">{(correctAnswers * (20 / scoreTotal)).toFixed(2)} / 20</p>
                 <p className="scoreQuiz2"><strong>{Math.floor((correctAnswers / scoreTotal) * 100)} %</strong></p>
-                <p className="timeQuiz"><strong>Your time :</strong> {formatTime(time)}</p>
-                <button className="btn btn-info mr-2 " onClick={onAnswersCheck}>Check your answers</button>
+                <p className="timeQuiz"><strong>Your time :</strong> {formatTime(props.time)}</p>
+                <button className="btn btn-info mr-2 " onClick={props.onAnswersCheck}>Check your answers</button>
               </div>
             </div>
           </AccordionDetails>
--- Client/src/components/Task/DisplayTask.js
+++ Client/src/components/Task/DisplayTask.js
@@ -1,24 +1,38 @@
 import React, {useState} from 'react'
 import Accordion from "@material-ui/core/Accordion";
 import './DisplayTask.css'
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
 
 function DisplayTask() {
-  const [task, setTask] = useState(
+    const [questionFile, setQuestionFile] = useState("")
+
+    const [task, setTask] = useState(
       [{
         Title: "React Hook",
         Theme: "seance 1",
         questionTitle: "question 1",
         QuestionFile: 'https://www.practiceportuguese.com/wp-content/uploads/2020/06/asking-questions-800x534.jpg',
-        QuestionResponseFile:''
+        QuestionResponseFile:questionFile
       }]
   )
-    const changeTaskFile = (text, i) => {
-        var newTask = [...task];
-        newTask[i].QuestionResponseFile = text;
-        setTask(newTask);
-        console.log(newTask)
+    const handleChangeStatus = ({ meta, file }, status) => {
+        console.log(meta, file, status);
+        if (status === "done") {
+            setQuestionFile(questionFile.concat(file));
+            console.log("hassen"+questionFile)
+        }
+        if (status === "removed") {
+            let questionFile = questionFile.slice();
+            questionFile = questionFile.filter((u) => {
+                return u !== file;
+            });
+            setQuestionFile(questionFile);
+            console.log("hassen"+questionFile)
+        }
     }
-  return (
+
+    return (
       <div className="DisplayQuestionBox">
         <Accordion>
             {task.map((task,index)=>(
@@ -41,11 +55,16 @@ function DisplayTask() {
                     <div className="task-response">
 
                         <h1 className="tasktitle">Response</h1>
-                        <div className="wrapper">
+                        {/*<div className="wrapper">
                             <div className="file-upload">
                                 <input type="file" name="fileresponse" multiple HTMLInputElement={task.QuestionResponseFile} onChange={(e)=>{changeTaskFile(e.target.files,index)}}/>
                                 <i className="fa fa-arrow-up"/>
                             </div>
+                        </div>*/}
+                        <div>
+                            <Dropzone onChangeStatus={handleChangeStatus}
+                                styles={{ dropzone: { minHeight: 120, maxHeight: 250 } }}
+                            />
                         </div>
                         <div className="saveadd">
                             <button className="btn btn-success" type="submit" onClick={()=>{}}>Send</button>
