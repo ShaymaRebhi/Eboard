eae84f1afda0e9714e9f68acc5f5e940e4d18d21
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-04-03 02:01:25 +0100

    Change Add Quiz function

--- Server/Controllers/QuizController.js
+++ Server/Controllers/QuizController.js
@@ -16,39 +16,15 @@ exports.deleteQuiz = async (req,res) =>{
         .catch(err => res.status(400).json(err));
 }
 
-
-
-exports.AddQuiz = async(req,res)=>{
-
-    const _Option = new Option({
-        OptionText : req.body.OptionText,
-        IsValid :req.body.IsValid
-    });
-
-   await _Option.save();
-    const optionID = _Option._id;
-    const _Question = new Question({
-        questionText : req.body.questionText,
-        Required :req.body.Required,
-        Score :req.body.Score,
-        Options :optionID,
-    });
-
-    await _Question.save();
-    const questionID = _Question._id;
-    const _Quiz = new Quiz({
-        Title : req.body.Title,
-        Theme :req.body.Theme,
-        Description :req.body.Description,
-        Questions :questionID,
-    });
-    await _Quiz.save((err, quiz) => {
-                        if (err) return res.status(503).json({error: err});
-                        if (quiz) return res.status(200).json({
-                            success:true,
-                            id:quiz._id,
-                            message:'quiz Created',
-                        });
+exports.AddQuiz = async(req,res) => {
+    const newQuiz = new Quiz(req.body)
+    await newQuiz.save((err, quiz) => {
+        if (err) return res.status(503).json({error: err});
+        if (quiz) return res.status(200).json({
+            success: true,
+            id: quiz._id,
+            message: 'Quiz Created'
+        });
     })
 }
 
